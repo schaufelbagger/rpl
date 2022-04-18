2834310bf8fcf850c644e6c92f49e40f38914280
alexyai <alexyai@gmx.at>
2022-04-18 22:54:05 +0200

    adds sending of DIO messages reimplements initialisation adds RPL constants and protocol-specific variables

--- model/rpl.cc
+++ model/rpl.cc
@@ -23,9 +23,11 @@
 #include "ns3/log.h"
 
 #include "ns3/trickle-timer.h"
+#include "ns3/nstime.h"
 #include "ns3/inet-socket-address.h"
 #include "ns3/socket-factory.h"
 #include "ns3/ipv6-raw-socket-factory.h"
+#include "ns3/ipv6-packet-info-tag.h"
 #include "ns3/boolean.h"
 #include "ns3/uinteger.h"
 #include<iostream>
@@ -39,31 +41,100 @@ NS_OBJECT_ENSURE_REGISTERED(RoutingProtocol);
 
 RoutingProtocol::RoutingProtocol ()
 {
-  NS_LOG_UNCOND("rpl constructor\n\n");
+  NS_LOG_DEBUG("--- RPL constructor ---");
 
-    
-  //DisHeader disHeader;
-  DisHeader head;
-  head.SetFlags (2);
-  std::cout<<"The Value of 'type' is "<< unsigned(head.GetType());
-  std::cout<<std::endl;
-  std::cout<<"The Value of 'code' is "<< unsigned(head.GetCode()) <<std::endl;
-  std::cout<<"The Value of 'flags' is "<< unsigned(head.GetFlags()) <<std::endl;
+  /*Ptr<Packet> packet = Create<Packet> ();
+  DisHeader inDisHeader;
+  RplIcmpv6Header inRplIcmpv6Header;
+  RplIcmpv6Header outRplIcmpv6Header;
+  DisHeader outDisHeader;
+  inDisHeader.SetFlags (3);
+  std::cout<<"---Input---"<< std::endl;
+  inDisHeader.Print (std::cout);
+  std::cout <<std::endl;
+  std::cout<<"The Value of 'type' is "<< unsigned(inRplIcmpv6Header.GetType())<<std::endl;
+  std::cout<<"The Value of 'code' is "<< unsigned(inRplIcmpv6Header.GetCode()) <<std::endl;
+  std::cout<<"The Value of 'flags' is "<< unsigned(inDisHeader.GetFlags()) <<std::endl;
+
+  //packet->AddHeader (inOption);
+  packet->AddHeader (inDisHeader);
+  packet->AddHeader (inRplIcmpv6Header);
+
+  packet->RemoveHeader (outRplIcmpv6Header);
+  packet->RemoveHeader (outDisHeader);
+  //packet->RemoveHeader (outOption);
+
+  std::cout <<std::endl;
+  std::cout<<"---Output---"<< std::endl;
+  outRplIcmpv6Header.Print (std::cout);
+  std::cout <<std::endl;
+  std::cout<<"The Value of 'type' is "<< unsigned(outRplIcmpv6Header.GetType())<<std::endl;
+  std::cout<<"The Value of 'code' is "<< unsigned(outRplIcmpv6Header.GetCode()) <<std::endl;
+  std::cout<<"The Value of 'flags' is "<< unsigned(outDisHeader.GetFlags()) <<std::endl;
+  //outDisHeader = outRplHeader;
+
+  //std::cout<<"The Value of 'flags' is "<< unsigned(outHeader.GetFlags()) <<std::endl;
 
   RplHeaderOption option;
   std::vector<uint8_t> testdata;
+  uint8_t rawPrefix[16];
   for (int i = 0; i < 5; i++) {
     testdata.push_back(i);
   }
+  std::fill(rawPrefix, rawPrefix+16, 0);
+  rawPrefix[0] = 0xFF;
+  rawPrefix[1] = 0xF0;
+
+  Ipv6Prefix objectPrefix (rawPrefix);
   
-  option.SetDagMetricContainer(testdata);
-  option.Print();
+  //option.SetDagMetricContainer(testdata);
+  //option.SetPadN (2);
+  option.SetRouteInformation (0b00011000, 0x01020304, objectPrefix);
+  option.Print();*/
 
 }
 
-void RoutingProtocol::SetInstanceId(uint16_t instanceId)
+
+void RoutingProtocol::DoInitialize ()
 {
-  m_instanceId = instanceId;
+  NS_LOG_DEBUG(this);
+  m_initialized = true;
+
+  for (uint32_t i = 0 ; i < m_ipv6->GetNInterfaces (); i++)
+  {
+    RegisterSockets (i);
+  }
+
+
+  m_trickleTimer.SetFunction (&RoutingProtocol::ExpireTimer, this);
+
+  if (m_isRoot)
+  {
+    NS_ABORT_MSG_IF (m_mop > 3, "Mode of Operation is invalid");
+    if (m_mop >= 2)
+    {
+      m_isStoring = true;
+    }else{
+      m_isStoring = false;
+    }
+  }
+
+  if (m_isRoot)
+  {
+    m_rank = ROOT_RANK;
+    m_isGrounded = true;
+    m_prf = 0;
+    // [RFC 6550, 8.3.1]
+    m_trickleTimer.Enable ();
+
+    // TODO send DIS
+  }
+
+  std::cout<<"Is Root: "<< +m_isRoot <<std::endl;
+  std::cout<<"MOP: "<< +m_mop <<std::endl;
+  std::cout<<"Instance ID: "<< +m_instanceId <<std::endl;
+
+  Ipv6RoutingProtocol::DoInitialize ();
 }
 
 TypeId RoutingProtocol::GetTypeId (void)
@@ -92,48 +163,171 @@ Ptr<Ipv6Route> RoutingProtocol::RouteOutput (Ptr< Packet > p,
   const Ipv6Header& header,
   Ptr< NetDevice > oif,
   Socket::SocketErrno & sockerr)
-
 {
 
   //TODO 
   Ptr<Ipv6Route> rtentry;
   rtentry = Create<Ipv6Route> ();
+
+  Ipv6Address destination = header.GetDestination ();
+
+  if (destination.IsMulticast ())
+  {
+    // Note:  Multicast routes for outbound packets are stored in the
+    // normal unicast table.  An implication of this is that it is not
+    // possible to source multicast datagrams on multiple interfaces.
+    // This is a well-known property of sockets implementation on
+    // many Unix variants.
+    // So, we just log it and fall through to LookupStatic ()
+    NS_LOG_LOGIC ("RouteOutput (): Multicast destination");
+  }
+  rtentry = Lookup (destination, true, oif);
+  if (rtentry)
+  {
+    sockerr = Socket::ERROR_NOTERROR;
+  }
+  else
+  {
+    sockerr = Socket::ERROR_NOROUTETOHOST;
+  }
   return rtentry;
 }
 
 void RoutingProtocol::SetIpv6 (Ptr<Ipv6> ipv6)
 {
-  NS_ASSERT (ipv6 != 0);
-  NS_ASSERT (m_ipv6 == 0);
-  NS_LOG_DEBUG ("Created rpl::RoutingProtocol");
-  //TODO init code
+  NS_LOG_FUNCTION (this << ipv6);
 
+  NS_ASSERT (m_ipv6 == 0 && ipv6 != 0);
+  uint32_t i = 0;
   m_ipv6 = ipv6;
-}
 
-void RoutingProtocol::RecvRpl (Ptr<Socket> socket)
+  for (i = 0; i < m_ipv6->GetNInterfaces (); i++)
+    {
+      if (m_ipv6->IsUp (i))
+        {
+          NotifyInterfaceUp (i);
+        }
+      else
+        {
+          NotifyInterfaceDown (i);
+        }
+    }
+
+  Simulator::ScheduleNow (&RoutingProtocol::Start, this);
+}
+void RoutingProtocol::Start ()
 {
 
+}
+void RoutingProtocol::Receive (Ptr<Socket> socket)
+{
+  NS_LOG_FUNCTION (this << socket);
+
+  Address sender;
+  Ptr<Packet> packet = socket->RecvFrom (sender);
+  Inet6SocketAddress senderAddr = Inet6SocketAddress::ConvertFrom (sender);
+  NS_LOG_INFO ("Received " << *packet << " from " << senderAddr);
+
+  Ipv6Address senderAddress = senderAddr.GetIpv6 ();
+
+  /*Ipv6PacketInfoTag interfaceInfo;
+  if (!packet->RemovePacketTag (interfaceInfo))
+  {
+    NS_ABORT_MSG ("No incoming interface on RPL message, aborting.");
+  }
+  uint32_t incomingIf = interfaceInfo.GetRecvIf ();
+  Ptr<Node> node = this->GetObject<Node> ();
+  Ptr<NetDevice> dev = node->GetDevice (incomingIf);
+  uint32_t ipInterfaceIndex = m_ipv6->GetInterfaceForDevice (dev);
+
+  SocketIpv6HopLimitTag hoplimitTag;
+  if (!packet->RemovePacketTag (hoplimitTag))
+  {
+    NS_ABORT_MSG ("No incoming Hop Count on RPL message, aborting.");
+  }
+  uint8_t hopLimit = hoplimitTag.GetHopLimit ();*/
+
+  int32_t interfaceForAddress = m_ipv6->GetInterfaceForAddress (senderAddress);
+  if (interfaceForAddress != -1)
+  {
+    NS_LOG_LOGIC ("Ignoring a packet sent by myself.");
+    return;
+  }
+
+  Ipv6Header ipv6;
+  packet->RemoveHeader (ipv6);
+  NS_ABORT_MSG_UNLESS (ipv6.GetNextHeader () == Ipv6Header::IPV6_ICMPV6, "The received Packet is not an ICMPv6 packet");
+  RplIcmpv6Header rplIcmpv6Header;
+
+  packet->RemoveHeader (rplIcmpv6Header);
+  NS_ABORT_MSG_UNLESS (rplIcmpv6Header.GetType () == 155, "Received ICMPv6 Message with type other than 155");
+switch (rplIcmpv6Header.GetCode ())
+    {
+    case TYPE_DIS:
+      NS_ABORT_MSG ("TODO add what to do upon receiving base message with code "<< rplIcmpv6Header.GetCode ());
+      break;
+    case TYPE_DIO:
+      NS_ABORT_MSG ("TODO add what to do upon receiving base message with code "<< rplIcmpv6Header.GetCode ());
+      /*DioHeader dioHeader;
+      packet->RemoveHeader (dioHeader);*/
+      break;
+    case TYPE_DAO:
+      NS_ABORT_MSG ("TODO add what to do upon receiving base message with code "<< rplIcmpv6Header.GetCode ());
+      break;
+    case TYPE_DAO_ACK:
+      NS_ABORT_MSG ("TODO add what to do upon receiving base message with code "<< rplIcmpv6Header.GetCode ());
+      break;
+    case TYPE_CC:
+      NS_ABORT_MSG ("TODO add what to do upon receiving base message with code "<< rplIcmpv6Header.GetCode ());
+      break;
+    default:
+      NS_ABORT_MSG ("Unrecognised base message code");
+    }
 }
 
 void RoutingProtocol::NotifyInterfaceUp (uint32_t interface)
 {
+  // Initialize function may be not executed before the interface goes up
 
-  NS_LOG_UNCOND("rpl interface up\n\n");
+  NS_LOG_FUNCTION (this << interface);
 
-  TypeId tid = TypeId::LookupByName ("ns3::Ipv6RawSocketFactory");
+  /*TypeId tid = TypeId::LookupByName ("ns3::Ipv6RawSocketFactory");
   Ptr<Socket> m_socket = Socket::CreateSocket (GetObject<Node> (), tid);
 
   NS_ASSERT (m_socket);
 
   m_socket->SetAttribute ("Protocol", UintegerValue(Ipv6Header::IPV6_ICMPV6));
-  m_socket->SetRecvCallback (MakeCallback (&RoutingProtocol::RecvRpl, this));
+  m_socket->SetRecvCallback (MakeCallback (&RoutingProtocol::Receive, this));
+  */
 
+  if (!m_initialized)
+  {
+    return;
+  }
 
-  
+  RegisterSockets (interface);
+
 }
+
 void RoutingProtocol::NotifyInterfaceDown (uint32_t interface)
-{}
+{
+  NS_LOG_FUNCTION (this << interface);
+
+  // TODO remove all routes that are assosiated with this interface
+
+  for (SocketListI iter = m_unicastSocketList.begin (); iter != m_unicastSocketList.end (); iter++ )
+  {
+    NS_LOG_INFO ("Checking socket for interface " << interface);
+    if (iter->second == interface)
+    {
+      NS_LOG_INFO ("Removed socket for interface " << interface);
+      iter->first->Close ();
+      m_unicastSocketList.erase (iter);
+      break;
+    }
+  }
+
+}
 void RoutingProtocol::NotifyAddAddress (uint32_t interface, Ipv6InterfaceAddress address)
 {}
 void RoutingProtocol::NotifyRemoveAddress (uint32_t interface, Ipv6InterfaceAddress address)
@@ -145,7 +339,113 @@ void RoutingProtocol::NotifyRemoveRoute (Ipv6Address dst, Ipv6Prefix mask, Ipv6A
 void RoutingProtocol::PrintRoutingTable (Ptr<OutputStreamWrapper> stream, Time::Unit unit) const
 {}
 
+void RoutingProtocol::RegisterSockets (uint32_t interface)
+{
+  bool sendSocketFound = false;
+  for (SocketListI iter = m_unicastSocketList.begin (); iter != m_unicastSocketList.end (); iter++ )
+  {
+    if (iter->second == interface)
+    {
+      sendSocketFound = true;
+      break;
+    }
+  }
 
+  bool activeInterface = false;
+  if (m_interfaceExclusions.find (interface) == m_interfaceExclusions.end ())
+  {
+    activeInterface = true;
+    m_ipv6->SetForwarding (interface, true);
+  }
+
+  for (uint32_t j = 0; j < m_ipv6->GetNAddresses (interface); j++)
+  {
+    Ipv6InterfaceAddress address = m_ipv6->GetAddress (interface, j);
+
+    if (address.GetScope() == Ipv6InterfaceAddress::LINKLOCAL && sendSocketFound == false && activeInterface == true)
+    {
+      NS_LOG_LOGIC ("RPL: adding sending socket to " << address.GetAddress ());
+      TypeId tid = TypeId::LookupByName ("ns3::Ipv6RawSocketFactory");
+      Ptr<Socket> socket = Socket::CreateSocket (GetObject<Node> (), tid);
+      NS_ASSERT (socket);
+      socket->SetAttribute ("Protocol", UintegerValue(Ipv6Header::IPV6_ICMPV6));
+      Inet6SocketAddress local = Inet6SocketAddress (address.GetAddress ());
+      socket->BindToNetDevice (m_ipv6->GetNetDevice (interface));
+      socket->Bind (local);  // this is the src Address if the socket is used to send packets, as well as the Address the Socket listens to when receiving packets
+      socket->SetRecvCallback (MakeCallback (&RoutingProtocol::Receive, this));
+      //socket->SetIpv6RecvHopLimit (true);
+      //socket->SetRecvPktInfo (true);
+      m_unicastSocketList[socket] = interface;
+    }
+    //else if (address.GetScope() == Ipv6InterfaceAddress::GLOBAL)
+    //{
+    //  SendTriggeredRouteUpdate ();
+    //}
+  }
+
+  if (!m_multicastRecvSocket)
+  {
+    NS_LOG_LOGIC ("RPL: adding receiving socket");
+    TypeId tid = TypeId::LookupByName ("ns3::Ipv6RawSocketFactory");
+    m_multicastRecvSocket = Socket::CreateSocket (GetObject<Node> (), tid);
+    NS_ASSERT (m_multicastRecvSocket);
+    m_multicastRecvSocket->SetAttribute ("Protocol", UintegerValue(Ipv6Header::IPV6_ICMPV6));
+    Inet6SocketAddress local = Inet6SocketAddress (RPL_ALL_NODE);
+    m_multicastRecvSocket->Bind (local);  // this is the src Address if the socket is used to send packets, as well as the Address the Socket listens to when receiving packets
+    m_multicastRecvSocket->SetRecvCallback (MakeCallback (&RoutingProtocol::Receive, this));
+    //m_multicastRecvSocket->SetIpv6RecvHopLimit (true);
+    //m_multicastRecvSocket->SetRecvPktInfo (true);
+  }
+}
+
+Ptr<Ipv6Route> RoutingProtocol::Lookup (Ipv6Address dst, bool setSource, Ptr<NetDevice> interface)
+{
+  Ptr<Ipv6Route> rtentry = Create<Ipv6Route> ();
+  // when sending on link-local multicast, there have to be interface specified
+  if (dst.IsLinkLocalMulticast ())
+  {
+    NS_ASSERT_MSG (interface, "Try to send on link-local multicast address, and no interface index is given!");
+    rtentry = Create<Ipv6Route> ();
+    rtentry->SetSource (m_ipv6->SourceAddressSelection (m_ipv6->GetInterfaceForDevice (interface), dst));
+    rtentry->SetDestination (dst);
+    rtentry->SetGateway (Ipv6Address::GetZero ());
+    rtentry->SetOutputDevice (interface);
+    return rtentry;
+  }
+
+  NS_ABORT_MSG ("RoutingProtocol::Lookup (...): TODO implement everything that is not a multicast!");
+}
+
+void RoutingProtocol::ExpireTimer (void)
+{
+  // send DIO
+  NS_LOG_LOGIC ("RPL: Send DIO Broadcast due to Trickle Timer expiration");
+  Ptr<Packet> packet = Create<Packet> ();
+  DioHeader dioHeader (m_instanceId, m_dodagVersionNumber, m_rank, m_isGrounded, m_mop, m_prf, m_dtsn, 0, 0, m_dodagId);
+  RplIcmpv6Header rplIcmpv6Header (TYPE_DIO);
+
+  packet->AddHeader (dioHeader);
+  packet->AddHeader (rplIcmpv6Header);
+
+
+
+
+  for (SocketListI iter = m_unicastSocketList.begin (); iter != m_unicastSocketList.end (); iter++ )
+  {
+    uint32_t interface = iter->second;
+
+    if (m_interfaceExclusions.find (interface) == m_interfaceExclusions.end ())
+    {
+      NS_LOG_DEBUG ("SendTo: " << +packet);
+      iter->first->SendTo (packet, 0, Inet6SocketAddress (RPL_ALL_NODE));
+    }
+  }
+  //m_multicastRecvSocket->SendTo (packet, 0, Inet6SocketAddress (RPL_ALL_NODE));
+
+
+  //sendRplPacket(createDio(), DIO, Ipv6Address::ALL_NODES_1, uniform(0, 1));
+  m_trickleTimer.Stop ();
+}
 
 }
 }
--- model/rpl.h
+++ model/rpl.h
@@ -23,13 +23,32 @@
 #include "ns3/ipv6-routing-protocol.h"
 #include "ns3/ipv6-route.h"
 #include "ns3/node.h"
+#include "ns3/trickle-timer.h"
 
 #include "rpl-header.h"
+#include "rpl-header-option.h"
+#include "rpl-objective-function.h"
+
+#include <memory>
 
 namespace ns3 {
 namespace rpl {
 
+#define RPL_DEFAULT_INSTANCE 0
 
+#define RPL_ALL_NODE "ff02::1a"
+
+// Trickle timer parameter [RFC6550, 8.3.1]
+#define DEFAULT_DIO_INTERVAL_MIN 0x03
+#define DEFAULT_DIO_INTERVAL_DOUBLINGS 0x03
+#define DEFAULT_DIO_REDUNDANCY_CONSTANT 0x14
+
+#define DEFAULT_DAO_DELAY 1
+//#define DEFAULT_INIT_DODAG_VERSION 0
+
+
+
+enum RplMop_e : uint8_t {MOP_NO_DOWNWARD_ROUTES=0, MOP_NON_STORING=1, MOP_STORING_NO_MULTICAST=2, MOP_STORING_MULTICAST=3};
 
 ///
 /// \ingroup rpl
@@ -41,6 +60,8 @@ class RoutingProtocol : public Ipv6RoutingProtocol
 public:
 
   RoutingProtocol();
+
+  void DoInitialize ();
   /**
    * \brief Get the type ID.
    * \return The object TypeId.
@@ -54,24 +75,11 @@ public:
   MulticastForwardCallback mcb,
   LocalDeliverCallback lcb,
   ErrorCallback ecb);
-
   Ptr<Ipv6Route> RouteOutput (Ptr< Packet > p,
   const Ipv6Header& header,
   Ptr< NetDevice > oif,
   Socket::SocketErrno & sockerr);
-
-
-
   void SetIpv6 (Ptr<Ipv6> ipv6);
-
-  ///\name Receive control packets
-  //\{
-  /**
-   * Receive and process control packet
-   * \param socket input socket
-   */
-  void RecvRpl (Ptr<Socket> socket);
-
   void NotifyInterfaceUp (uint32_t interface);
   void NotifyInterfaceDown (uint32_t interface);
   void NotifyAddAddress (uint32_t interface, Ipv6InterfaceAddress address);
@@ -80,19 +88,137 @@ public:
   void NotifyRemoveRoute (Ipv6Address dst, Ipv6Prefix mask, Ipv6Address nextHop, uint32_t interface, Ipv6Address prefixToUse = Ipv6Address::GetZero ());
   void PrintRoutingTable (Ptr<OutputStreamWrapper> stream, Time::Unit unit = Time::S) const;
 
-  void SetInstanceId(uint16_t instanceId);
+  /**
+   * \brief Set if node is Root
+   * \param isRoot if node is Root
+   */
+  void SetRoot (bool isRoot)
+  {
+    m_isRoot = isRoot;
+  }
+  /**
+   * \brief Get if node is Root
+   * \return if node is Root
+   */
+  bool GetRoot ()
+  {
+    return m_isRoot;
+  }
+  /**
+   * \brief Set the Mode of Operation
+   * \param mop the Mode of Operation
+   */
+  void SetMop (RplMop_e mop)
+  {
+    m_mop = mop;
+  }
+  /**
+   * \brief Get the Mode of Operation
+   * \return the Mode of Operation
+   */
+  RplMop_e GetMop ()
+  {
+    return m_mop;
+  }
+  /**
+   * \brief Set the Instance Id
+   * \param instanceId the Instance Id
+   */
+  void SetInstanceId (uint8_t instanceId)
+  {
+    m_instanceId = instanceId;
+  }
+  /**
+   * \brief Get the Instance Id
+   * \return the Instance Id
+   */
+  uint8_t GetInstanceId ()
+  {
+    return m_instanceId;
+  }
+  /**
+   * \brief Set the Interface Exclusions
+   * \param instanceId the Interface Exclusions
+   */
+  void SetInterfaceExclusions (std::set<uint32_t> exceptions)
+  {
+    //NS_LOG_FUNCTION (this);
+    m_interfaceExclusions = exceptions;
+  }
+  /**
+   * \brief Get the Interface Exclusions
+   * \return the Interface Exclusions
+   */
+  std::set<uint32_t> GetInterfaceExclusions () const
+  {
+    return m_interfaceExclusions;
+  }
+
+
+
+
 
 private:
+
+  void Start ();
+  /**
+   * Receive and process control packet
+   * \param socket input socket
+   */
+  void Receive (Ptr<Socket> socket);
+
+  /**
+   * \brief adds sending and receive sockets
+   * 
+   * \param interface the interface on which the link-local address will be added as sending address
+   */
+  void RegisterSockets (uint32_t interface);
+
+  /**
+   * \brief Lookup in the forwarding table for destination.
+   * 
+   * \param dst destination address of the packet
+   * \param setSource set source address in the route
+   * \param interface output interface if any (put 0 otherwise)
+   * \return Ptr<Ipv6Route> Ipv6Route to route the packet to reach dest address
+   */
+  Ptr<Ipv6Route> Lookup (Ipv6Address dst, bool setSource, Ptr<NetDevice> interface = 0);
+
+  /**
+   * \brief Fires when trickle timer expires
+   */
+  void ExpireTimer (void);
+
+
+  bool m_initialized = false;
   /// IP protocol
   Ptr<Ipv6> m_ipv6;
-  // RPL Identifiers
-  uint8_t m_instanceId = 0;
-  Ipv6Address m_dodagid;
+  std::set<uint32_t> m_interfaceExclusions; // Set of excluded interfaces
+  /// Socket list type
+  typedef std::map< Ptr<Socket>, uint32_t> SocketList;
+  /// Socket list type iterator
+  typedef std::map<Ptr<Socket>, uint32_t>::iterator SocketListI;
+  /// Socket list type const iterator
+  typedef std::map<Ptr<Socket>, uint32_t>::const_iterator SocketListCI;
+  /// Sockets
+  SocketList m_unicastSocketList; // list of sockets for unicast messages (socket, interface index)
+  Ptr<Socket> m_multicastRecvSocket; // multicast receive socket
+  /// RPL Identifiers
+  uint8_t m_instanceId = RPL_DEFAULT_INSTANCE;
+  Ipv6Address m_dodagId;
   uint8_t m_dodagVersionNumber;
   uint16_t m_rank;
-  bool m_isGrounded;
-
+  uint8_t m_dtsn;
+  RplMop_e m_mop;
+  uint8_t m_prf : 3;
+  bool m_isStoring;
+  bool m_isRoot = false;
+  bool m_isLeaf = false;
+  bool m_isGrounded = false;
+  /// Timer
+  TrickleTimer m_trickleTimer = TrickleTimer (Time(DEFAULT_DIO_INTERVAL_MIN), DEFAULT_DIO_INTERVAL_DOUBLINGS,DEFAULT_DIO_REDUNDANCY_CONSTANT);
 };
+
 
 }   // rpl
 }   // ns3
