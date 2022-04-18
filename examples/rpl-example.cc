758046df38b4255281f609ab1b41b08012f40ca4
alexyai <alexyai@gmx.at>
2022-04-18 22:50:19 +0200

    revises example adds libaries to CMakeLists.txt

--- examples/rpl-example.cc
+++ examples/rpl-example.cc
@@ -1,36 +1,153 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-
+/*
+ * Copyright (c) 2022 COPYRIGHTHOLDER
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Alexander Baranyai <e1525251@student.tuwien.ac.at>
+ */
 #include "ns3/core-module.h"
 #include "ns3/rpl-helper.h"
+#include "ns3/network-module.h"
 #include "ns3/internet-module.h"
+#include "ns3/internet-apps-module.h"
 #include "ns3/internet-stack-helper.h"
+#include "ns3/mobility-module.h"
+#include "ns3/propagation-module.h"
+#include "ns3/sixlowpan-module.h"
+
+#include "ns3/lr-wpan-module.h"
+#include "ns3/yans-wifi-helper.h"
+
+//#define USE_WIFI
+#define USE_SIXLOWPAN
+//#define USE_APPLICATION
 
 using namespace ns3;
 
 
 int main (int argc, char *argv[])
 {
-    bool verbose = true;
+  // parameters
+  bool verbose = true;
+  int numberOfNodes = 2;
+  // distance of nodes
+  int step = 20;
+#ifdef USE_APPLICATION
+  uint32_t packetSize = 10;
+  uint32_t maxPacketCount = 5;
+  Time interPacketInterval = Seconds (1.);
+#endif
 
-    CommandLine cmd (__FILE__);
-    cmd.AddValue ("verbose", "Tell application to log if true", verbose);
+  /// network
+  /// nodes used in the example
+  NodeContainer nodes;
+  /// devices used in the example
+  NetDeviceContainer devices;
+  /// interfaces used in the example
+  Ipv6InterfaceContainer interfaces;
 
-    cmd.Parse (argc,argv);
+  CommandLine cmd (__FILE__);
+  cmd.AddValue ("verbose", "Tell application to log if true", verbose);
 
-    NS_LOG_UNCOND("rpl example\n\n");
-    // create 2 nodes
-    NodeContainer nodes;
-    nodes.Create(2);
-    // install TCP/IP & RPL
-    RplHelper rpl;
-    InternetStackHelper internetv6;
-    internetv6.SetRoutingHelper(rpl);
-    internetv6.Install(nodes);
+  cmd.Parse (argc,argv);
+
+  NS_LOG_UNCOND("rpl example\n\n");
+
+  // ---------------- Create Nodes -------------------------------
+
+  // create nodes
+  nodes.Create(numberOfNodes);
+  MobilityHelper mobility;
+  mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
+                                 "MinX", DoubleValue (0.0),
+                                 "MinY", DoubleValue (0.0),
+                                 "DeltaX", DoubleValue (step),
+                                 "DeltaY", DoubleValue (0),
+                                 "GridWidth", UintegerValue (numberOfNodes),
+                                 "LayoutType", StringValue ("RowFirst"));
+  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  mobility.Install (nodes);
+
+  // ---------------- Create Devices -------------------------------
+  /*
+  WifiMacHelper wifiMac;
+  wifiMac.SetType ("ns3::AdhocWifiMac");
+  YansWifiPhyHelper wifiPhy;
+  YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default ();
+  wifiPhy.SetChannel (wifiChannel.Create ());
+  WifiHelper wifi;
+  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager", "DataMode", StringValue ("OfdmRate6Mbps"), "RtsCtsThreshold", UintegerValue (0));
+  devices = wifi.Install (wifiPhy, wifiMac, nodes); 
+  if (verbose)
+  {
+    wifiPhy.EnablePcapAll (std::string ("rpl"));
+  }*/
+
+  LrWpanHelper lrWpanHelper;
+  // Add and install the LrWpanNetDevice for each node
+  NetDeviceContainer lrwpanDevices = lrWpanHelper.Install(nodes);
+
+  // Fake PAN association and short address assignment.
+  // This is needed because the lr-wpan module does not provide (yet)
+  // a full PAN association procedure.
+  lrWpanHelper.AssociateToPan (lrwpanDevices , 1);
+
+#ifdef USE_SIXLOWPAN
+  SixLowPanHelper sixlowpan;
+  devices = sixlowpan.Install (lrwpanDevices ); 
+#else
+  devices = lrwpanDevices;
+#endif
+  // ---------------- Install Internet Stack -------------------------------
+  // install TCP/IP & RPL
+  RplHelper rpl;
+  // you can configure RPL attributes here using rpl.Set(name, value)
+  InternetStackHelper stack;
+  stack.SetRoutingHelper(rpl);
+  stack.Install(nodes);
+  rpl.AssignRoot (NodeContainer (nodes.Get (0)) );
 
 
-    Simulator::Run ();
-    Simulator::Destroy ();
-    return 0;
+  Ipv6AddressHelper ipv6;
+  ipv6.SetBase (Ipv6Address ("2001:2::"), Ipv6Prefix (64));
+  Ipv6InterfaceContainer deviceInterfaces;
+  deviceInterfaces = ipv6.Assign (devices);
+
+
+  // ---------------- Install Applications -------------------------------
+#ifdef USE_APPLICATION
+  Ping6Helper ping6;
+
+  ping6.SetLocal (deviceInterfaces.GetAddress (0, 1));
+  ping6.SetRemote (deviceInterfaces.GetAddress (1, 1));
+
+  ping6.SetAttribute ("MaxPackets", UintegerValue (maxPacketCount));
+  ping6.SetAttribute ("Interval", TimeValue (interPacketInterval));
+  ping6.SetAttribute ("PacketSize", UintegerValue (packetSize));
+  ApplicationContainer apps = ping6.Install (nodes.Get (0));
+
+  apps.Start (Seconds (1.0));
+  apps.Stop (Seconds (5.0));
+#endif
+
+  Simulator::Stop (Seconds (5));
+  
+  Simulator::Run ();
+  Simulator::Destroy ();
+
+  return 0;
 }
 
 
