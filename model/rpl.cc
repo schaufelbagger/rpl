797e70250c346ad9c612cb35035424a8783ed0c6
alex yai <alexyai@gmx.at>
2024-07-04 18:26:36 +0200

    adds Tommaso Pecorella for data path validation author

--- model/rpl.cc
+++ model/rpl.cc
@@ -16,6 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * Author: Alexander Baranyai <alexander.baranyai@gmx.net>
+ * Author: Tommaso Pecorella <tommaso.pecorella@unifi.it> (data path validation)
  */
 #include "rpl.h"
 
